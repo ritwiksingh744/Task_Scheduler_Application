59081f9d38f0dfff2494dfa7a952222bc42f1e00
ritwiksingh744 <70218759+ritwiksingh744@users.noreply.github.com>
2023-03-11 15:52:46 +0530

    startup class setup

---
+++ Startup.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Task_Scheduler_Application
+{
+    public class Startup
+    {
+        private IConfiguration _config;
+        private string hostingEnv;
+
+
+        public Startup(Microsoft.AspNetCore.Hosting.IWebHostEnvironment env)
+        {
+            hostingEnv = env.EnvironmentName;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath);
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            RegisterDependecies(services);
+        }
+
+        private void RegisterDependecies(IServiceCollection services)
+        {
+        }
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (hostingEnv == "local")
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseCors("Anonymous");
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+
+    }
+}
